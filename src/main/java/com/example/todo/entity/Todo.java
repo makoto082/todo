23043c3568bb5bc35b3abe8664b00ca0528ff5cf
m_makoto <m.makoto9922@gmail.com>
2025-10-31 23:51:37 +0900

    add H2 db config

--- src/main/java/com/example/todo/entity/Todo.java
+++ src/main/java/com/example/todo/entity/Todo.java
@@ -21,7 +21,7 @@ public class Todo {
 	private boolean done = false;
 	
 	@Column(nullable = false)
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = LocalDateTime.now();
 	
 	public Long getId() { return id; }
 	public void setId(Long id) { this.id = id; }
